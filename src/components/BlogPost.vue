cd94f9b5017d1f05a04758763d4b657d7c632c69
darian_br97 <darianbrgz@gmail.com>
2022-07-12 21:30:43 -0400

    added blog order through the resize event of the window

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,71 +1,85 @@
 <template>
-	<div class="blog-presentacion w-full grid grid-cols-3 items-center">
-		<div
-			class="
-				introduction-blog
-				w-full
-				h-full
-				flex
-				justify-center
-				items-center
-			"
-		>
-			<div
-				class="
-					w-[50%]
-					h-[50%]
-					flex flex-col
-					justify-center
-					items-center
-					gap-4
-				"
-			>
-				<h2 class="text-4xl">{{ post.title }}</h2>
-				<p>{{ post.blogHTML }}</p>
-				<router-link
-					v-if="post.welcomeScreen"
-					to="#"
-					class="flex items-center gap-2"
-					>Login/Register
-					<img
-						src="../assets/Icons/arrow-right-light.svg"
-						alt="arrow icon"
-						class="w-4 h-4"
-				/></router-link>
-				<router-link v-else to="#" class="flex items-center gap-2"
-					>View The Post
-					<img
-						src="../assets/Icons/arrow-right-light.svg"
-						alt="arrow icon"
-						class="w-4 h-4"
-				/></router-link>
-			</div>
-		</div>
-		<div class="image-blog col-span-2">
-			<img :src="putImage" alt="cover photo" class="w-full h-auto" />
-		</div>
-	</div>
+  <div
+    class="
+      w-full
+      grid grid-cols-1 grid-rows-2
+      md:grid-cols-3 md:grid-rows-1
+      items-center
+    "
+    :class="onDesktop ? 'blog-order' : ''"
+  >
+    <div
+      class="
+        introduction-blog
+        w-full
+        h-full
+        flex
+        justify-center
+        items-center
+        row-start-2 row-end-3
+        md:row-auto
+      "
+    >
+      <div
+        class="w-[50%] h-[50%] flex flex-col justify-center items-center gap-4"
+      >
+        <h2 class="text-4xl">{{ post.title }}</h2>
+        <p>{{ post.blogHTML }}</p>
+        <router-link
+          v-if="post.welcomeScreen"
+          to="#"
+          class="flex items-center gap-2"
+          >Login/Register
+          <img
+            src="../assets/Icons/arrow-right-light.svg"
+            alt="arrow icon"
+            class="w-4 h-4 invert"
+        /></router-link>
+        <router-link v-else to="#" class="flex items-center gap-2"
+          >View The Post
+          <img
+            src="../assets/Icons/arrow-right-light.svg"
+            alt="arrow icon"
+            class="w-4 h-4"
+        /></router-link>
+      </div>
+    </div>
+    <div class="image-blog row-start-1 row-end-2 md:col-span-2 md:row-auto">
+      <img :src="putImage" alt="cover photo" class="w-full h-auto bg-cover" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { useStore } from "../stores/store";
 export default {
-    name: "blogPost",
-    props: ["post"], 
-    setup() {
+  name: "blogPost",
+  props: ["post"],
+  setup() {
+    const store = useStore();
+
+    return { store };
+  },
+  computed: {
+    putImage({ post }) {
+      return `./src/assets/blogPhotos/${post.blogCoverPhoto}.jpg`;
     },
-    computed: {
-        putImage({post}) {
-            return `./src/assets/blogPhotos/${post.blogCoverPhoto}.jpg`
-        }
-    }
-}
+    onDesktop() {
+      return this.store.windowWidth >= 768;
+    },
+  },
+};
 </script>
 
 <style>
-	.blog-presentacion:nth-child(even) .introduction-blog {
-		order: 2;
-	}
-	.blog-presentacion:nth-child(even) .image-blog {
-		order: 1;
-	}
+.blog-order:nth-child(even) .introduction-blog {
+  grid-column-start: 3;
+  grid-column-end: 4;
+}
+.blog-order:nth-child(even) .image-blog {
+  grid-column-start: 1;
+  grid-column-end: 3;
+  grid-row-start: 1;
+  grid-row-end: 2;
+}
 </style>
