fa8fe23d81551de8be878efcdd73afd4d8e361db
darian_br97 <darianbrgz@gmail.com>
2022-07-14 09:47:08 -0400

    Create ToggleEditBtn.vue

---
+++ src/components/ToggleEditBtn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="absolute w-full flex justify-end">
+    <div class="w-[104px] flex items-center justify-between mr-8">
+      <span class="font-semibold">Edit Post</span>
+      <div class="w-8 h-4 shadow rounded-3xl bg-white">
+        <div
+          @click="toggleEdit"
+          class="
+            w-4
+            h-4
+            bg-gray-900
+            rounded-full
+            border-2 border-gray-300
+            cursor-pointer
+            transition-all
+            hover:shadow hover:border-4
+          "
+          :class="editPost ? 'translate-x-4' : ''"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from "../stores/store";
+export default {
+  setup() {
+    const store = useStore();
+
+    const toggleEdit = () => (store.editPost = !store.editPost);
+
+    return { store, toggleEdit };
+  },
+  computed: {
+    editPost() {
+      return this.store.editPost;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
