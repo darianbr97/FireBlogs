9da4c3e723bdc60adbe12ba3214bff93abd944df
darian_br97 <darianbrgz@gmail.com>
2022-07-14 09:50:37 -0400

    added custom screen's sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     content: ["./src/**/*.{html,js,vue}"],
     theme: {
         extend: {},
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1075px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
     },
     plugins: [],
 };
