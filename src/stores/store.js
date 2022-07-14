a2363e431f1c5234bc41efdbaa95321577070c9d
darian_br97 <darianbrgz@gmail.com>
2022-07-14 09:48:07 -0400

    added editPost and sampleBlogCards variables

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,6 +3,29 @@ import { defineStore } from "pinia";
 export const useStore = defineStore({
   id: "store",
   state: () => ({
-    windowWidth: 0
+    windowWidth: 0,
+    editPost: false,
+    sampleBlogCards: [
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-1",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #2",
+        blogCoverPhoto: "stock-2",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #3",
+        blogCoverPhoto: "stock-3",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #4",
+        blogCoverPhoto: "stock-4",
+        blogDate: "May 1, 2021",
+      },
+    ],
   }),
 });
