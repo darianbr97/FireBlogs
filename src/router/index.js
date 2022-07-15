6ce8b391b671479f1d14c12d2b772d121ede3a66
darian_br97 <darianbrgz@gmail.com>
2022-07-14 22:16:36 -0400

    added login, register and forgotPassword routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Blogs from "../views/Blogs.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import ForgotPassword from "../views/ForgotPassword.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,13 +12,48 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: Home,
+            meta: {
+                title: "HOME"
+            }
         },
         {
             path: "/blogs",
             name: "blogs",
             component: Blogs,
+            meta: {
+                title: "BLOGS"
+            }
+        },
+        {
+            path: "/login",
+            name: "Login",
+            component: Login,
+            meta: {
+                title: "LOGIN"
+            }
+        },
+        {
+            path: "/register",
+            name: "Register",
+            component: Register,
+            meta: {
+                title: "REGISTER"
+            }
+        },
+        {
+            path: "/forgot-password",
+            name: "ForgotPassword",
+            component: ForgotPassword,
+            meta: {
+                title: "Forgot Password"
+            }
         },
     ],
 });
+
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title} | FireBlogs`;
+    next();
+}) 
 
 export default router;
