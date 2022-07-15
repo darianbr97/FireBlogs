1c7f9a6b649e4671fbaa675a288228d343ff61d6
darian_br97 <darianbrgz@gmail.com>
2022-07-14 22:15:16 -0400

    added overflow hidden

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div
     id="container"
-    class="relative w-full min-h-screen flex flex-col overflow-x-hidden"
+    class="relative w-full min-h-screen flex flex-col overflow-hidden"
   >
-    <Navigation />
+    <Navigation v-if="NavFootEnabled" />
     <RouterView />
-    <Footer />
+    <Footer v-if="NavFootEnabled" />
   </div>
 </template>
 
 <script>
+import { useRoute } from "vue-router";
 import Navigation from "./components/Navigation.vue";
 import Footer from "./components/Footer.vue";
+import { ref, watch } from "@vue/runtime-core";
 export default {
-  name: "app",
+  name: "App",
   components: { Navigation, Footer },
+  setup() {
+    const route = useRoute();
+    const NavFootEnabled = ref(true);
+
+    function checkRoute() {
+      if (
+        route.name === "Login" ||
+        route.name === "Register" ||
+        route.name === "ForgotPassword"
+      ) {
+        NavFootEnabled.value = false;
+      } else NavFootEnabled.value = true;
+    }
+
+    watch(() => route.name, checkRoute);
+
+    return { NavFootEnabled };
+  },
 };
 </script>
 
