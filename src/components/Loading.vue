f057fbdc1840143cc1bfb57eac4f8de70acdac69
darian_br97 <darianbrgz@gmail.com>
2022-07-16 21:55:36 -0400

    Create Loading.vue

---
+++ src/components/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="
+      absolute
+      w-full
+      h-full
+      z-50
+      top-0
+      bg-black/75
+      flex
+      justify-center
+      items-center
+    "
+  >
+    <div
+      class="
+        w-[60px]
+        h-[60px]
+        rounded-full
+        border-[3px] border-transparent border-t-white border-b-white
+        animate-spin
+      "
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
