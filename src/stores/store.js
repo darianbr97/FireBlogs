5a0a69b2aaf647a63608f3b8b7d4c18f6c2cb8c2
darian_br97 <darianbrgz@gmail.com>
2022-07-17 23:53:51 -0400

    added user info

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,4 +1,7 @@
+import { getAuth } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore/lite";
 import { defineStore } from "pinia";
+import { db } from "../firebase/firebaseInit";
 
 export const useStore = defineStore({
   id: "store",
@@ -27,5 +30,25 @@ export const useStore = defineStore({
         blogDate: "May 1, 2021",
       },
     ],
+    user: null,
+    profileEmail: null,
+    profileFirstName: null,
+    profileLastName: null,
+    profileUserName: null,
+    profileId: null,
+    profileInitials: null,
   }),
+  actions: {
+    async getCurrentUser () {
+      const userData = await doc(db, "users", getAuth().currentUser.uid);
+      const dbResults = await getDoc(userData);
+      this.profileId = dbResults.id;
+      this.profileEmail = dbResults.data().email;
+      this.profileFirstName = dbResults.data().firstName;
+      this.profileLastName = dbResults.data().lastName;
+      this.profileUserName = dbResults.data().userName;
+      this.profileInitials = this.profileFirstName.match(/(\b\S)/g).join('') + this.profileLastName.match(/(\b\S)/g).join('');
+      console.log(dbResults)
+    }
+  }
 });
