abc381ce914620be7bc0d51c6d7b6f51a2706f24
darian_br97 <darianbrgz@gmail.com>
2022-07-19 16:23:32 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,19 +4,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-// let app;
-// getAuth().onAuthStateChanged(() => {
-//     if(!app) {
-//         app = createApp(App)
-//         app.use(createPinia());
-//         app.use(router);
-        
-//         app.mount("#app");
-//     }
-// })
-
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
